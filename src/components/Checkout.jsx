9c1f4dfe23b80639e56cb8a0fb1389244f7d51f8
baskara restu <baskararw10@gmail.com>
2023-05-03 04:16:22 +0700

    checkout trial and error

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProductById } from '../features/products/productSlice'
+import { useParams } from 'react-router'
 
 function Checkout() {
+  const dispatch = useDispatch()
+  const { id } = useParams()
+  const selectedProduct = useSelector((state) => state.product.selectedProduct)
+  const [formData, setFormData] = useState({
+    name: '',
+    price: '',
+  })
+
+  const { name, price } = formData
+
+  useEffect(() => {
+    dispatch(fetchProductById(id)) // Fetch the product by ID
+  }, [dispatch, id])
+
+  useEffect(() => {
+    setFormData({
+      name: selectedProduct?.name || '',
+      price: selectedProduct?.price || '',
+    })
+  }, [selectedProduct])
   return (
-    <div class="h-screen flex flex-col overflow-hidden">
-      <div>test</div>
+    <div class="w-full h-full p-6 bg-white border border-blue-200 shadow">
+      <div className="grid grid-cols-1 gap-5">
+        <div>
+          <h2>Checkout Item</h2>
+        </div>
+        <div>
+          <div>item</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Checkout;
+export default Checkout
